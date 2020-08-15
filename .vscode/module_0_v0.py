940edd383017b046f631fd7ff5b165ab58775b6d
Yanasla <jasyagold893@gmail.com>
2020-08-15 22:18:37 +0300

    1-versus

--- .vscode/module_0_v0.py
+++ .vscode/module_0_v0.py
@@ -1,29 +1,13 @@
 import numpy as np
+count = 0                            # счетчик попыток
+number = np.random.randint(1,101)    # загадали число
+print ("Загадано число от 1 до 100")
 
-def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
-    count_ls = []
-    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1,101, size=(1000))
-    for number in random_array:
-        count_ls.append(game_core(number))
-        score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return(score)
-
-def game_core(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
-    count = 1
-    predict = np.random.randint(1,101)
-    while number != predict:
-        count+=1
-        if number > predict: 
-            predict += 1
-        elif number < predict: 
-            predict -= 1
-        return(count) # выход из цикла, если угадали
-
-
-
-score_game(game_core)
+while True:                        # бесконечный цикл
+    predict = int(input())         # предполагаемое число
+    count += 1                     # плюсуем попытку
+    if number == predict: break    # выход из цикла, если угадали
+    elif number > predict: print (f"Угадываемое число больше {predict} ")
+    elif number < predict: print (f"Угадываемое число меньше {predict} ")
+        
+print (f"Вы угадали число {number} за {count} попыток.")
