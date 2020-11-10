8cfaddbca1aa3d97e881db8a706fd6865b0bc426
Yanasla <jasyagold893@gmail.com>
2020-11-10 15:37:24 +0200

    4

--- readfilebylines.py
+++ readfilebylines.py
@@ -3,7 +3,7 @@
          #print(line)
 #f.close()
 f = open('StudentsPerformance.csv')
-males = 0
+'''males = 0
 females = 0
 for line in f:
     info = line.split(',')
@@ -12,5 +12,25 @@ for line in f:
         females += 1
     elif gender == 'male':
         males +=1
-print('Мальчиков: {}, девочек: {}'.format(males, females))
+print('Мальчиков: {}, девочек: {}'.format(males, females))'''
+
+'''res_list = []
+for line in f:
+    info = line.split(',')
+    pe = info[2][1:-1]
+    if pe not in res_list: 
+        res_list.append(pe)
+print("Unique elements of the list using append():n")    
+for item in res_list: 
+    print(item)
+print(len(res_list)-1)'''
+
+LS = 0
+for line in f:
+    info = line.split(',')
+    lunch = info[3][1:-1]
+    if lunch == 'standart':
+        LS += 1
+    
+print('Полноценный обед получили: {}.format(LS)')
 
